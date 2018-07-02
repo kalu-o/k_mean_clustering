b89359cc9c1c8825bc62f04b969abd26009b3ecf
Udochukwu Kalu Ogbureke <yookosdub2@gmail.com>
2018-07-02 15:53:44 +0200

    Add files via upload
    Added the header file

---
+++ kmeans.h
@@ -0,0 +1,25 @@
+#ifndef KMEANS_H
+#define KMEANS_H
+#include "util.h"
+
+/*
+ *	Some simple, useful #define's
+ */
+
+typedef struct cluster
+{
+    float centroid[DIM_FEATURE];
+    char name_cluster[MAX_NUM_CHAR];
+    int c_count;
+    float c_sum;
+}cluster_t;
+
+/*
+ *	Function prototypes
+ */
+int get_euclidean_dist(const float*, const float*, float*);
+int assign_cluster(input_node_t*, const cluster_t*);
+int update_cluster_centroid(input_list_t*, cluster_t*);
+int kmeans_algorithm(input_list_t*, cluster_t*);
+int write_cluster_name(char*, const input_list_t*);
+#endif // KMEANS_H
